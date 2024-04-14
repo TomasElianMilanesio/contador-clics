74d4ae49e23b97914f8dc726284e48e4ee5f5f03
unknown <tomas43443@gmail.com>
2024-04-14 11:45:08 -0300

    Frist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logotipo from './imagenes/logo.jpg';
+import Boton from './componentes/Boton.js';
+import Contador from './componentes/Contador.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numClics: 0
+    };
+    this.manejarClic = this.manejarClic.bind(this);
+    this.reiniciarContador = this.reiniciarContador.bind(this);
+  }
+
+  manejarClic() {
+    this.setState(({numClics}) => ({numClics: numClics + 1}));
+  }
+  
+  reiniciarContador() {
+    this.setState({numClics: 0});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="logo-contenedor">
+          <img 
+            className="logo"
+            src={logotipo}
+            alt="Logo del contador de clics"/>
+         </div>
+         <div className="contenedor-principal">
+            <Contador
+            numClics={this.state.numClics}/>
+            <Boton
+            texto="Clic"
+            esBotonDeClic={true}
+            manejarClic={this.manejarClic}/>
+            <Boton
+            texto="Reiniciar"
+            esBotonDeClic={false}
+            manejarClic={this.reiniciarContador}/>
+        </div>
+      </div>
+    );
+
+  }
 }
+
 
 export default App;
